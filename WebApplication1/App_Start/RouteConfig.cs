6ef0cf5d86dcf867d807c6564087211bcf245590
nhab <n.habibollahi@yahoo.com>
2022-08-08 13:16:17 +0430

    Enabling Routes attributes

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace WebApplication1
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(

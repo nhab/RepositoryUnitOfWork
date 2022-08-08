6731a96eb42b358e2096ac56c6cd8776a1eef764
nhab <n.habibollahi@yahoo.com>
2022-08-08 18:08:57 +0430

    To Create the view for each action in GameController ,In visual studio editor right click the action and select Add View.A dialogbox Apears to help you with the creation of its view.

---
+++ WebApplication1/Migrations/202208081313500_intDB.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Game",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Game");
+        }
+    }
+}

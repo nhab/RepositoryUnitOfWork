7d25a85f157ad8c762d59a3cca59ed0c3c6b9c01
nhab <n.habibollahi@yahoo.com>
2022-08-08 17:27:12 +0430

    -latest Entityframewrok nuget installed -enable-migration ran in the PM - rout.MapMvcAttributeRoutes(); added to App_start/Routonfig.cs -Game Model added to models -GameContext  Added -seed added to Migrations/Configuration.cs -Connectionstring added to Web.config

---
+++ WebApplication1/DAL/GameContext.cs
@@ -0,0 +1,18 @@
+﻿using GameApplication.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+
+namespace GameApplication.DAL
+{
+    public class GameContext : DbContext
+    {
+        public GameContext() : base("GameContext")
+        {
+        }
+        public DbSet<Game> Games { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebApplication1/Models/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GameApplication.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

c5074e5051a2c0912233d8c12e11c34961d727a4
nhab <n.habibollahi@yahoo.com>
2022-08-08 17:30:55 +0430

    DBContext GeneralRepository and UnitOfWork classes added

---
+++ WebApplication1/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GameApplication.Models;
+namespace GameApplication.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private GameContext context = new GameContext();
+        private GenericRepository<Game> gameRepository;
+        public GenericRepository<Game> GameRepository
+        {
+            get
+            {
+                return this.gameRepository ?? new GenericRepository<Game>(context);
+            }
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
